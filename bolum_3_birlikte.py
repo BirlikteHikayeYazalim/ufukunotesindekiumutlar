27407ae38e7bb613a84df9b8ca0953710489e779
Volkan Kepoğlu <vkepoglu@gmail.com>
2015-07-05 23:08:22 +0300

    Create bolum_3_birlikte.py

---
+++ bolum_3_birlikte.py
@@ -0,0 +1,65 @@
+4	BÖLÜM-3 BİRLİKTE
+Ufuk, öğretmeninin mezarına geldiğinde başucunda Ayşe dua ediyordu. Duayı bitirmesini bekledi. Yanına yaklaştı, ellerinden tuttu ve Ufuk’u görünce coşkuyla gülen Ayşe’nin gözlerinin içine bakarken bir anda öngörü gördü. Ayşe ne gördüğünü sordu.
+-	Güzel bir evde ben, sen ve çocuklar vardı.
+-	Harika, ben de seni seviyorum, Ufuk. Bak sana birini tanıştırayım. Bu benim babam. Yani annemin ikinci kocası, Kemal. İkinizden de ricam sevgili annemi bana anlatmanız.
+-	Elbette anlatırız.
+# Ufuk Ayşe’ye öğretmen olan anneyi ve Kemal Ayşe’ye eş olan anneyi anlatır. 
+# Kemal, Ufuk ve Ayşe birlikte Erzurum’dan İstanbul’a giderken konaklanacak, gezip görülecek bir iki yere uğranacaktır;
+# Yozgat’taki yetimhaneden arkadaşı Müge’ye uğradılar. Mantı yediler. Cumhurbaşkanı eşinin yemek kitabı ve yabancı misafirlere sunulan geleneksel yemeklerimizden bahsedilir. Erkeğin kalbine giden yol yemeklerden gider. Benim kalbimi mi kazanmaya çalışıyorsun. Arkadaşıyla yetimhaneden hatırladıkları anılardan bahsederler. Neşeli bir sohbet olur.
+# Her şey birbiriyle ilişkilidir muhabbeti olur. Müge, kendi mesleğinden örnek verir; önce resim yapan daha sonra matematiğe yönlendirilen ve geometride katkı bulunan ünlü bilim adamı Escher’in hikayesini anlatacağım.
+# Konya’ya uğrayıp etli ekmek yerler; türbeyi ziyaret ederler. Belediyenin ulaşım projesindeki hassasiyetten bahsedilir; tramvay güzergahında türbe ve tarihi kent dokusu kısmından geçerken sarsıntıyı önleyecek önlem alındığından ve görüntü kirliliğini önlemek için elektrik direksiz bir hat geliştirildiği, tramvaya elektrik yerleştirilen pil/akülerle yapılacaktır. 
+# Afyona uğrayıp sucuk ekmek yerler. Üstüne kaymaklı ekmek kadayıfı yerler. Ayşe, vişneli ekmek tatlısı yedi. Vişnenin faydaları.
+# Belki başka bir yer örneği daha; Ilgaz olabilir.
+# Yolculuk sırasında yapılan konuşmalarla birbirlerini daha iyi tanırlar, flört ederler ve birbirlerinden hoşlanırlar. Ayşe iş teklifi kabul eder.
+# Birlikte işyerinde yeni bir şehir kurma projesini geliştirmeye başlarlar. İşler başta zordur, sonra iyi gider, büyürler ve yeni personel alırlar.
+5	BÖLÜM-4 HAYAT GÜNEŞİ
+# Gönül öğretmen hastanede doğurduğu bebeğinin gelmesini bekliyor. Bebeğinin kaçırıldığını ve eşinin öldüğünü öğrendikten sonraki durumu anlatılmalı. 
+# Girdiği depresyon anlatılır.
+# Depresyondan nasıl çıktığından bahsedilir. Bebeğini tekrar bulma umudu.
+# Hayata tutunma çabası ve Tedavi görmesi. 
+# Bu bölümün sonu ilkokul öğretmeni olarak Ufuk’a hocalık yapmaya başlaması ile biter.
+6	BÖLÜM-5 GİZEM ÇÖZÜLÜYOR
+-	Benimle evlenir misin?
+-	Evet, seni seviyorum. Hem de çok, Ufuk. Ufuk. Ufuk. İyi misin? Ne oldu? Rengin soldu. Ufuk, Ufuk. Ufuk. 
+-	Evet, evet. Ben de seni seviyorum ve iyiyim. Ayrıca babanın babamla görev aldığı baskında bir bombanın patlamasıyla öldüğü anı ve annenin hastanede önce eşini daha sonra seni aynı günde kaybettiğini öğrendiğinde hissettiği acıyı gördüm.
+-	Babanın arabanın altına konan bir bombada öldüğünü söylemiştim.
+-	Evet, çünkü bana öyle söylediler.
+-	Ve eğer aynı devriye arabasında öldüğü söylenen diğer polis benim babam ise babamla baban iyi bir dosttu demektir.
+-	Ama annem öğretmenim yani annenin soyadını biliyordu. Ve bana hiçbir şey söylemedi. 
+-	Ya annem soyadını değiştirdi, gerçi bunu yapması için bir sebep yok. Ya da öngörün doğru. Babalarımız birbirlerini tanımıyorlardı.
+-	Babalarımız bir patlamada öldü ama araba içinde değil bir baskında. Farklı birimlerin katıldığı geniş çaplı ortak bir operasyon olabilir.
+-	Peki araba konusunda niye yalan söylediler?
+-	Suçlunun kim olduğunun peşine düşmeyelim diye. Baskının kimler için yapıldığı kısmen bilinir. Ama arabaya bomba kondu denilirse faili meçhulü işaret eder ve bulunmasının zor olduğu yargısı daha kolay kabul edilir.
+-	Öngöründe neyle ilgili bir baskın olduğunu hatırlıyor musun?
+-	Patlamayla birlikte etrafa saçılan kağıt paralar ve toz bulutu.
+-	Toz belki de esrardır.
+-	Sanırım babamın öldüğü haberini vermeye gelen arkadaşı Hamza Yiğit’i ziyaret etme vakti geldi.
+# Babalarının nasıl ve kimler tarafından ne amaçla öldürüldüğünü araştırmaya başlarlar.
+# Biraz heyecan ve macera katmak için bu konu macera şeklinde detaylandırılarak anlatılacak.
+# Ve ayrıca konu mantığı içerisinde baskınla ilişkilendirilerek cevaplanması gereken sorular;
+# Gönül öğretmenin kızı Ayşe’yi neden ve kimler kaçırdı?
+# Ayşe’yi neden yetimhaneye terk ettiler?
+7	BÖLÜM-6 SON
+#???
+8	KAYNAKÇA
+1.	Okumanın sağlayabileceği faydalardan, okuma yazarlıkla ilgili istatistiki bilgilerden ve okuyucu dili sınıflandırılması konularında Selahattin Yaylamaz’ın yazdığı “Okuma Zekası” adlı kitaptan yararlanılmıştır.
+2.	Yaşam Başarısı kavramı, Doğan Cüceloğlu’nun yazdığı “Başarıya Götüren Aile” adlı kitaptan alınmıştır.
+3.	“Yeşil Elmas” adlı hikaye Nil Kepoğlu tarafından yazılmıştır ve izni ile eklenmiştir.
+4.	“İçimizdeki Deniz” adlı hikaye Sena Demirel tarafından yazılmıştır ve izni ile eklenmiştir.
+5.	M. C. Escher ile ilgili bilgiler Haziran 2015 tarihli Bilim ve Gelecek Dergisi Escher’in Biyografisi ve Matematiksel Yönü adlı yazılardan faydalanılarak kullanılmıştır. 
+6.	
+9	KARAKTER LİSTESİ
+1.	Ufuk Sevinçoğlu – Başkahraman
+2.	Mehmet Sevinçoğlu – Ufuk’un Babası
+3.	Leyla Sevinçoğlu – Ufuk’un Annesi
+4.	Mustafa Demir – Ufuk’un Dayısı
+5.	Coşkun Ender – Turan Şirketinde Ufuk’un Müdürü
+6.	Hamza Yiğit – Mehmet’in meslektaşı
+7.	Zehra Çelik – Ufuk’un İlkokul aşkı
+8.	Mahmut Çelik – Zehra’nın kocası
+9.	Ayşe Öztürk (Kağan) – Ufuk’un aşkı, ikinci kahraman
+10.	Gönül Kağan – Ufuk’un ilkokul öğretmeni, Ayşe’nin annesi, Ahmet’in eşi, Kemal’in ikinci eşi
+11.	Ahmet Kağan – Gönül’ün ilk eşi, Ayşe’nin babası
+12.	Kemal Düzgün – Gönül’ün ikinci eşi, Ayşe’nin üvey babası
+13.	Ekrem Çiğdem – Yetimhane müdürü
+14.	Müge Koyuncu – Ayşe’nin Yetimhaneden dostu
