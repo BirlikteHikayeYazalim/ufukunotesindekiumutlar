616ef79135a6cfb57df2b08564ebd47239a551a6
Volkan Kepoğlu <vkepoglu@gmail.com>
2015-07-05 23:02:08 +0300

    Create bolum_2_ayse.py

---
+++ bolum_2_ayse.py
@@ -0,0 +1,44 @@
+3	BÖLÜM-2 AYŞE
+
+3.1	Yetimhane
+# Ayşe Öztürk, İzmir’deki yetimhaneye terk edilmiş ve burada büyümüş. Annesi ve babası kim ve hayatta mı bilmiyor.
+# İyi bir müdürün yönettiği bir yetimhanede yaşayabileceği ilginç olaylar
+# Müdür Ekrem Çiğdem karakterinin tarifi yapılır; Ekrem Bey, yetimlerin 18 yaşında hayata atılacaklarını bildiği için iyi bir eğitim almalarını sağlar.
+# Ayşe yetimhanede sıkı okuryazarlığı sayesinde kültürlü bir kız olarak yetişir.
+# Yurttan ayrılma korkusu
+3.2	Merhaba, Yeni Hayat
+# Yurttan ayrılma ve kız arkadaşı Müge Koyuncu ile birlikte yeni hayat kurma mücadelesi. 
+# Müge karakterinin tasviri yapılır.
+# Birlikte eve çıkarlar. Üniversiteyi kazanana kadar burada birlikte yaşarlar.
+# Gazeteci olmak ister. Birkaç görüşme yapar boş pozisyon olmadığı için ret edilir. Gelecek gazetesi üniversite mezunu olmadığı için stajyer olarak işe kabul eder. İlginç bir haber getirmeyi başardığı için kadrolu olarak işe alınır; Fatma Hanım’ın anlattığı işaret dili tercümanlığıyla ilgili sağır bir engellinin hikayesini anlatacağım. Bu sayede gazetede tam  zamanlı muhabir olarak çalışmaya başlar.
+# Başka haber olabilecek ilginç olay ve hikayelerden bahsedilebilir. 
+# Müge Özel ders vererek geçimini sağlar.
+3.3	Üniversite Hayatı
+# Hem çalışıp hem de üniversite sınavına hazırlanırlar, sınav stresi yaşarlar ve kazanırlar.
+# Burs kazanarak yurtta kalmaya başlar. İlginç kız yurdu hikayeleri. 
+# Ayşe, 3 boyutlu tasarım ve çizim yapma konusunda başarılıdır ve yarışmada ödül kazanır.
+# Ayşe, makine mühendisliğinden mezun olur. 
+# Müge, matematik öğretmeni olur ve Kayseri’ye tayini çıkar. 
+3.4	İş Hayatı
+# Yarışma ödül başarısından dolayı Hibrid araba modeli üretimi yapmak isteyen bir firma Ayşe’yi işe alır. Bu iş yerinde çalışarak birlikte iş yapmayı öğrenir.
+# Gazetede gönüllü yazar olarak yazı yazmaya devam eder.
+3.5	Mucize
+# Bir iki sene sonra yetimhane müdürü telefon ederek görüşmek istediğini ve yurda gelmesini ister
+-	Merhaba müdür bey, benimle bu kadar acil ne hakkında konuşmak istiyorsunuz?
+-	Otur, kızım. Sana verilmek üzere isimsiz bir mektup aldım. Mektubu biraz araştırdım. Postaneye 26 yıl sonra yetimhane ulaştırılmak üzere tam da doğum günün olarak kutladığımız bu günde yani senin yetimhaneye bırakıldığın gününde verilmiş. Mektubun üzerinde senin adın ve soyadın yazıyordu. Bu mektubu kim verdi ise senin buraya bırakıldığını ve burada büyüdüğünü bilen biri yazmıştır. Seni buraya getiren biri veya belki de ailenden biri olabilir.
+-	Mektupta ne yazıyor?
+-	Açıp okumadım. Senin yerine okumamı ister misin?
+-	Hayır, verin, ben okurum.
+-	Ne yazıyor mektupta?
+-	Annemin yaşadığını, ondan intikam almak için beni kaçırdığını ve yetimhaneye verdiğini yazıyor.
+-	Annen kimmiş?
+-	Gönül Kağan. 
+-	Gönül Kağan mı? Sepette senin soyadın Öztürk olarak yazıyordu.
+-	Bulamasınlar diye soyadımı değiştirmiş olabilirler veya annem kızlık soyadını kullanmış olabilir. İlkokul öğretmeniymiş.
+-	Senin adına çok sevindim. Annen yaşıyor olabilir. Belki de, dikkatli olmalısın. Bu mektup gerçek de olmayabilir. Onu bulmaya çalışacak mısın?
+-	Evet, en azından bir ipucum var artık. Denemem gerek.
+-	Onu bulmak için yardım etmemi ister misin?
+-	İnşallah hala yaşıyordur, ama sağ olun, ben nasıl bulacağımı biliyorum. Öğretmen bir can dostumdan yardım isteyeceğim. Onun tanıdığı meslektaşları sayesinde annemi bulabilirim. Çok teşekkür ederim. Lütfen yeni bir haber alırsanız mutlaka haber verin.
+-	Tabii kızım, haberleşelim. İrtibatı kopartma. Görüşürüz, selametle.
+-	Kendinize iyi bakın.
+# Öğretmen kız arkadaşından telefonla yardım ister. Mesleki bağlantıları sayesinde kız arkadaşı annesinin en son çalıştığı Erzincan’daki köyü bulur. Arkadaşına teşekkür eder, vedalaşır ve annesini bulmak üzere otobüse binerek Erzincan’a yola çıkar.
